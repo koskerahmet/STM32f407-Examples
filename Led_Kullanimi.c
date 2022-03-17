d0d9476defb27b5a96d4078347e73c1f69200fba
Ahmet <77435649+koskerahmet@users.noreply.github.com>
2022-03-17 15:44:31 +0300

    Update Led_Kullanimi.c

--- Led_Kullanimi.c
+++ Led_Kullanimi.c
@@ -11,10 +11,10 @@ int main(void)
 
   while (1)
   {
-     HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, GPIO_PIN_SET);
-	   HAL_Delay(2000);
+     HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, GPIO_PIN_SET);  //Ledlerin Pinlerini HIGH Yapmak
+     HAL_Delay(2000);
 	   
-     HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, GPIO_PIN_RESET); 
+     HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, GPIO_PIN_RESET); //Ledlerin Pinlerini LOW Yapmak
      HAL_Delay(2000);
   }
 }
@@ -28,6 +28,6 @@ void LED_GPIO_Init(void)
   GPIO_InitStruct_Led.Pin 	= GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15; 
   GPIO_InitStruct_Led.Mode 	= GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct_Led.Pull 	= GPIO_NOPULL;
-  GPIO_InitStruct_Led.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct_Led.Speed     = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct_Led);
 }
